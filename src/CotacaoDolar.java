10e5e7cba75daae09f98a3db924ad690cfb4b948
willjp04 <sostenes655@gmail.com>
2024-11-02 14:48:27 -0300

    commitando exercicio Cotação Dólar

---
+++ src/CotacaoDolar.java
@@ -0,0 +1,33 @@
+import javax.sound.midi.Soundbank;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CotacaoDolar {
+
+    public static void main(String[] args) {
+        //Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares. Calcular e exibir o valor correspondente em Reais (R$).
+
+        Scanner scan = new Scanner(System.in);
+
+        float cotacaoDolar;
+        float dolares;
+        float total;
+
+        System.out.print("Digite o valor da cotação do dólar: " );
+
+        cotacaoDolar = scan.nextFloat();
+
+        System.out.println();
+
+        System.out.print("Digite a quantidade de dólares para converter em Reais: ");
+
+        dolares = scan.nextFloat();
+
+        total = cotacaoDolar * dolares;
+
+        System.out.println();
+
+        System.out.printf("O VALOR CONVERTIDO PARA REAIS É: R$ %.2f " , total);
+
+    }
+}
