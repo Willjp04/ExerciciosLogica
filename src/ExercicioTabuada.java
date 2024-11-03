9e1ab4271e4d8593de7effcf05f590b20745ec80
willjp04 <sostenes655@gmail.com>
2024-11-03 20:15:07 -0300

    Commit  exercicíos

--- src/ExercicioTabuada.java
+++ src/ExercicioTabuada.java
@@ -7,7 +7,25 @@ public class ExercicioTabuada {
         // Travar a digitação, no sentido de aceitar somente valores positivos.
         // Após a digitação, exibir a tabuada do valor solicitado, no intervalo de um a dez.
 
+        int valor = 1;
+        int numero;
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Insira o valor para calcular a tabuada: ");
+        numero = sc.nextInt();
+
+        while (numero <=0){
+            System.out.println("Valor invalido! Digite um valor positivo: ");
+            numero = sc.nextInt();
+        }
+
+        while (valor <= 10){
+            System.out.println("Resultado de: " + numero + "X" + valor + "= " + valor*numero);
+            valor++;
+
+        }
+        sc.close();
+
+
     }
 }
